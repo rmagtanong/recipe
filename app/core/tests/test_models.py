d449a1f8b5054f37dd32540690ae9a1c88b83adf
romeomaria.magtanong <romeomaria.magtanong@gcash.com>
2023-02-08 21:12:28 +0800

    Add Ingredients API

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -69,3 +69,13 @@ class ModelTests(TestCase):
         tag = models.Tag.objects.create(user=user, name='tag')
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredient'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
+        
